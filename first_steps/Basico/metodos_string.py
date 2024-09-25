475736874e99134dd319586a325aa8afebfb164b
Paulo Amorim <paulo.amorimh@gmail.com>
2024-09-25 11:26:30 -0300

    Inserir arquivos novos

---
+++ first_steps/Basico/metodos_string.py
@@ -0,0 +1,10 @@
+mensagem = 'Eu adoro comida japonesa'
+
+print(mensagem)
+print(mensagem.lower()) # Todas as letras em minusculo
+print(mensagem.upper()) # Todas as letras em maiusculo
+print(mensagem.capitalize()) # A primeira letra em maiusculo
+print(mensagem.find('c')) # Mostra o index da palavra a ser encontrada
+print(mensagem.find('adoro')) # mostra o index onde inicia a palavra. Quando o resultado for -1, significa que não foi encontrado
+print(mensagem.replace('a', 'e')) # troca as letras ou palavras em um texto
+print(mensagem.strip()) # Remove qualquer espaço antes do primeiro caracter
